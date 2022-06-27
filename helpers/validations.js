975de3c4685534dc952512216ea7f5056f18be79
VanehSann <vanessacbsantos22@gmail.com>
2022-06-27 18:46:05 -0300

    requisito 5

--- helpers/validations.js
+++ helpers/validations.js
@@ -15,6 +15,7 @@ const validateEmail = (email, response) => {
     }); 
   }
 };
+
 const validatePassword = (password, response) => {
   if (!password || password === '') {
  return response.status(400).json({
@@ -35,4 +36,98 @@ const loginMiddleware = (_request, response, next) => {
     next();
 };
 
-module.exports = { loginMiddleware };
+// Requisito 5
+
+const nameMiddleware = (_request, response, next) => {
+  const { name } = _request.body;
+  
+  if (!name || name === '') { 
+    return response.status(400).json({
+      message: 'O campo "name" é obrigatório',
+      
+    }); 
+  }
+  if (name.length < 3) {
+    return response.status(400).json(
+      {
+        message: 'O "name" deve ter pelo menos 3 caracteres', 
+      },
+    ); 
+}
+  next();
+};
+
+const ageMiddleware = (_request, response, next) => {
+  const { age } = _request.body;
+  
+  if (!age || age === '') { 
+    return response.status(400).json({
+      message: 'O campo "age" é obrigatório',
+    }); 
+  }
+  if (age < 18) {
+    return response.status(400).json(
+      {
+        message: 'A pessoa palestrante deve ser maior de idade',
+      },
+    ); 
+}
+  next();
+};
+
+const talkMiddleware = (_request, response, next) => {
+  const { talk } = _request.body;
+  
+  if (!talk || talk === '') { 
+    return response.status(400).json({
+      message: 'O campo "talk" é obrigatório',
+    }); 
+  }
+  next();
+};
+
+const watchedAtMiddleware = (_request, response, next) => {
+  const { talk: { watchedAt } } = _request.body;
+  
+  if (!watchedAt || watchedAt === '') { 
+    return response.status(400).json({
+      message: 'O campo "watchedAt" é obrigatório',
+    }); 
+  }
+ // Regex - 22.5 Express - Middlewares - Gabarito
+  const dateRegex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  if (!dateRegex.test(watchedAt)) {
+    return response.status(400).json(
+      {
+        message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+      },
+    ); 
+}
+  next();
+};
+
+const rateMiddleware = (_request, response, next) => {
+  const { talk: { rate } } = _request.body;
+  
+  if (!rate || rate === '') { 
+    return response.status(400).json({
+      message: 'O campo "rate" é obrigatório',
+    }); 
+  }
+  
+  if (rate <= 0 || rate >= 6) {
+    return response.status(400).json({
+        message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+      }); 
+}
+  next();
+};
+
+module.exports = { 
+      loginMiddleware,
+      nameMiddleware,
+      ageMiddleware,
+      talkMiddleware,
+      watchedAtMiddleware,
+      rateMiddleware,
+       };
--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const readFIle = require('./helpers/readFile');
-const { loginMiddleware } = require('./helpers/validations');
+const writeFIle = require('./helpers/writeFile');
+const { 
+  loginMiddleware,
+  nameMiddleware,
+  ageMiddleware,
+  talkMiddleware,
+  watchedAtMiddleware,
+  rateMiddleware,
+   } = require('./helpers/validations');
+const authorizationMiddleware = require('./helpers/authorization');
 
 const app = express();
 app.use(bodyParser.json());
@@ -28,9 +37,21 @@ app.get('/talker/:id', async (_request, response) => {
 });
 
 // Requisito 3 e 4
-app.post('/login', loginMiddleware, (request, response) => {  
+app.post('/login', loginMiddleware, (_request, response) => {  
   const token = crypto.randomBytes(8).join('').substring(0, 16); 
  return response.status(200).json({ token });
+});
+
+// Requisito 5
+app.post('/talker', authorizationMiddleware, nameMiddleware,
+ageMiddleware,
+talkMiddleware,
+watchedAtMiddleware,
+rateMiddleware, async (_request, response) => {
+  const results = await readFIle();
+    results.push({ id: results.length + 1, ..._request.body });
+    writeFIle(results);
+  return response.status(201).json({ id: results.length, ..._request.body });
 });
 
 // não remova esse endpoint, é para o avaliador funcionar

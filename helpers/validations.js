29abfe6ab702fd0084cd66be11550b4ce4ed70ad
VanehSann <vanessacbsantos22@gmail.com>
2022-06-29 19:06:17 -0300

    mudando a posicao do requisito 8 pra remover o conflito com o requisito 6

--- helpers/validations.js
+++ helpers/validations.js
@@ -108,16 +108,16 @@ const watchedAtMiddleware = (_request, response, next) => {
 
 const rateMiddleware = (_request, response, next) => {
   const { talk: { rate } } = _request.body;
-  if (!rate || rate === '') { 
-    return response.status(400).json({
-      message: 'O campo "rate" é obrigatório',
-    }); 
-  }
   if (rate < 1 || rate > 5) {
     return response.status(400).json({
         message: 'O campo "rate" deve ser um inteiro de 1 à 5',
       });
 }
+  if (!rate || rate === '') { 
+    return response.status(400).json({
+      message: 'O campo "rate" é obrigatório',
+    }); 
+  }
 
   next();
 };
